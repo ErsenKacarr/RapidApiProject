f4785febc78c0ce9de6286b9389d8b6a8db0488a
ErsenKacarr <ersenamzn@gmail.com>
2024-10-28 19:12:17 +0300

    Add project files.

---
+++ RapidApiProject/Controllers/HotelDetailController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+
+namespace RapidApiProject.Controllers
+{
+    public class HotelDetailController : Controller
+    {
+        public async Task<IActionResult> Index(string hotelID, string arrivalDate, string departureDate)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://booking-com15.p.rapidapi.com/api/v1/hotels/getHotelDetails?hotel_id={hotelID}&arrival_date={arrivalDate}&departure_date={departureDate}&languagecode=en-us&currency_code=EUR"),
+                Headers =
+    {
+        { "x-rapidapi-key", "e39e0ce586msh0fb7b2f29f24fe1p1134dfjsn5094ae8fdc5c" },
+        { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<HotelDetailViewModel>(body);
+                return View(value);
+            }
+        }
+    }
+}
---
+++ RapidApiProject/Models/HotelSearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RapidApiProject.Models
+{
+    public class HotelSearchViewModel
+    {
+        public string dest_id { get; set; }
+        public string city_name { get; set; }
+        public DateTime checkinDate { get; set; }
+        public DateTime checkoutDate { get; set; }
+        public int adultCount { get; set; }
+
+        public int roomCount { get; set; }
+
+    }
+}
